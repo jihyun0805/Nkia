78f64300b6ab511332061251c425e89488dfedbc
최준 <choijun0627@naver.com>
2026-04-24 11:19:23 +0900

    [S14P31S106-100] Feat : 활동 관련 엔티티 생성
    SalesActivity, SalesActivityRequest 엔티티 생성
    활동, 활동 요청 의미
    SalesActivity에서 사용되는 value object 클래스도 생성

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/salesactivity/salesactivity/entity/SalesActivityAttendee.java
@@ -0,0 +1,32 @@
+package com.nkia.Orbis.domain.salesactivity.salesactivity.entity;
+
+import com.nkia.Orbis.common.entity.BaseEntity;
+import com.nkia.Orbis.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SalesActivityAttendee extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sales_activity_id")
+    private SalesActivity salesActivity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
