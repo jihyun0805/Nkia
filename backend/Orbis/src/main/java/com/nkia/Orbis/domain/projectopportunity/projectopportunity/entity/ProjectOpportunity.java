d2e047f3d8e93e07204dce88e76bdb527926d99b
최준 <choijun0627@naver.com>
2026-04-24 11:00:42 +0900

    [S14P31S106-100] Feat : 사업 기회 관련 entity 생성
    사업 기회 관련 entity 생성
    entity와 VO는 같은 entity 패키지에 두는게 좋음
    ProjectOpportunity, ProjectOpportunityPartnerCompany,
    ProjectOpportunityProductModule 클래스 생성
    사업 기회 및 중간 entity 의미

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/projectopportunity/projectopportunity/entity/ProjectOpportunity.java
@@ -0,0 +1,55 @@
+package com.nkia.Orbis.domain.projectopportunity.projectopportunity.entity;
+
+import com.nkia.Orbis.common.entity.BaseEntity;
+import com.nkia.Orbis.domain.company.entity.Company;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectOpportunity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_company_id")
+    private Company customerCompany;
+
+    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectOpportunityPartnerCompany> partnerCompanies = new ArrayList<>();
+
+    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectOpportunityProductModule> productModules = new ArrayList<>();
+
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<SalesActivity> salesActivities = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Quotation> quotations = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<RfpAnalyzeResult> rfpAnalyzeResults = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Prb> prbs = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<BidResult> bidResults = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "projectOpportunity", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<OrderReport> orderReports = new ArrayList<>();
+}
