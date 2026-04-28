6d3bba114ddd9cebdca7bbb979b964bee738f652
여희림 <gmlfla0219@naver.com>
2026-04-28 13:32:48 +0900

    [S14P31S106-96] feat: SalesActivityController 구현

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/salesactivity/salesactivity/controller/SalesActivityController.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/salesactivity/salesactivity/controller/SalesActivityController.java
@@ -2,11 +2,14 @@ package com.nkia.Orbis.domain.salesactivity.salesactivity.controller;
 
 import com.nkia.Orbis.common.response.ApiResponse;
 import com.nkia.Orbis.domain.salesactivity.salesactivity.dto.request.SalesActivityCreateRequest;
+import com.nkia.Orbis.domain.salesactivity.salesactivity.dto.request.SalesActivityUpdateRequest;
 import com.nkia.Orbis.domain.salesactivity.salesactivity.service.SalesActivityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,16 @@ public class SalesActivityController {
     ) {
         Long salesActivityId = salesActivityService.create(request);
         return ApiResponse.success(salesActivityId);
+    }
+
+    @Operation(summary = "영업 활동 수정")
+    @PatchMapping("/{salesActivityId}")
+    public ApiResponse<Long> updateSalesActivity(
+            @PathVariable Long salesActivityId,
+            @RequestBody SalesActivityUpdateRequest request
+    ) {
+        return ApiResponse.success(
+                salesActivityService.updateSalesActivity(salesActivityId, request)
+        )
     }
 }
