e8d56ff643326df185b05c893ee41f92495a66b9
여희림 <gmlfla0219@naver.com>
2026-05-04 09:59:00 +0900

    [S14P31S106-172] feat: ProductModule Service 구현

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/productmodule/service/ProductModuleService.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/productmodule/service/ProductModuleService.java
@@ -1,5 +1,8 @@
 package com.nkia.Orbis.domain.productmodule.service;
 
+import com.nkia.Orbis.domain.productmodule.dto.request.ProductModuleCreateRequest;
+import com.nkia.Orbis.domain.productmodule.dto.response.ProductModuleResponse;
+import com.nkia.Orbis.domain.productmodule.entity.ProductModule;
 import com.nkia.Orbis.domain.productmodule.repository.ProductModuleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,18 @@ public class ProductModuleService {
     private final ProductModuleRepository productModuleRepository;
 
     @Transactional
-    public Produ
+    public ProductModuleResponse create(ProductModuleCreateRequest request) {
+        ProductModule productModule = ProductModule.create(
+                request.getProductClass(),
+                request.getProductGroup(),
+                request.getProductName(),
+                request.getLicenseStandard(),
+                request.getLicenseUnit(),
+                request.getUnitPrice()
+        );
+
+        productModuleRepository.save(productModule);
+
+        return ProductModuleResponse.from(productModule);
+    }
 }
