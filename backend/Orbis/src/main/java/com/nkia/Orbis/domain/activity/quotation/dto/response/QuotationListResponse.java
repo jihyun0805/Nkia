8736de30996efca84ca9ba5357397a7714cbbb91
여희림 <gmlfla0219@naver.com>
2026-05-04 15:13:59 +0900

    [S14P31S106-148] feat: QuotationList Response dto 생성

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/activity/quotation/dto/response/QuotationListResponse.java
@@ -0,0 +1,43 @@
+package com.nkia.Orbis.domain.activity.quotation.dto.response;
+
+import com.nkia.Orbis.domain.activity.quotation.entity.Quotation;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QuotationListResponse {
+    private Long id;
+
+    private String quotationCode;
+
+    private Long projectOpportunityId;
+
+    private LocalDate quotationDate;
+
+    private Long consumerTotalPrice;
+
+    private Long supplyTotalPrice;
+
+    private Long laborTotalPrice;
+
+    private Long totalPrice;
+
+    public static QuotationListResponse from(Quotation quotation) { // 수정됨
+        return QuotationListResponse.builder()
+                .id(quotation.getId())
+                .quotationCode(quotation.getQuotationCode())
+                .projectOpportunityId(
+                        quotation.getProjectOpportunity() == null
+                                ? null
+                                : quotation.getProjectOpportunity().getId()
+                )
+                .quotationDate(quotation.getQuotationDate())
+                .consumerTotalPrice(quotation.getConsumerTotalPrice())
+                .supplyTotalPrice(quotation.getSupplyTotalPrice())
+                .laborTotalPrice(quotation.getLaborTotalPrice())
+                .totalPrice(quotation.getTotalPrice())
+                .build();
+    }
+}
