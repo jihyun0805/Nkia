8108a10c58a8220b3b0db55c08d0526fcac4f662
여희림 <gmlfla0219@naver.com>
2026-05-04 10:10:43 +0900

    [S14P31S106-172] feat: ProductModule Controller 구현

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/productmodule/controller/ProductModuleController.java
@@ -0,0 +1,35 @@
+package com.nkia.Orbis.domain.productmodule.controller;
+
+import com.nkia.Orbis.common.response.ApiResponse;
+import com.nkia.Orbis.domain.productmodule.dto.request.ProductModuleCreateRequest;
+import com.nkia.Orbis.domain.productmodule.dto.response.ProductModuleResponse;
+import com.nkia.Orbis.domain.productmodule.service.ProductModuleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Product Module", description = "제품 모듈 관리 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/product-modules")
+public class ProductModuleController {
+    private final ProductModuleService productModuleService;
+
+    @Operation(summary = "제품 모듈 생성")
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductModuleResponse>> createProductModule(
+            @Valid
+            @RequestBody
+            ProductModuleCreateRequest request
+    ) {
+        ProductModuleResponse response = productModuleService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+    }
+}
