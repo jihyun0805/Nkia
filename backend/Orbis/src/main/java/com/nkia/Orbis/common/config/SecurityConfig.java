91b0a9eb969aea048be03e2c54fe4e458929d612
정서영 <jsy021202@naver.com>
2026-05-04 17:09:27 +0900

    [S14P31S106-190] fix : OCR 기능 오류 수정

--- backend/Orbis/src/main/java/com/nkia/Orbis/common/config/SecurityConfig.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/common/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
                         .requestMatchers("/user/signup/admin/**").permitAll() // 관리자 계정 생성만 임시 허용
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 허용
                         .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("/business-card-ocr").permitAll()
                         .requestMatchers("/business-card-ocr/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 API는 인증 필요
