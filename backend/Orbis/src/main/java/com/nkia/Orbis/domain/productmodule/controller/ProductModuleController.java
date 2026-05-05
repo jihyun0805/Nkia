3531a2b0c3e59916f6b256fee9f83f1ee6ec1318
여희림 <gmlfla0219@naver.com>
2026-05-05 22:13:57 +0900

    [S14P31S106-171] ProductModuleController update 기능 구현

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/productmodule/controller/ProductModuleController.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/productmodule/controller/ProductModuleController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,4 +55,14 @@ public class ProductModuleController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "제품 모듈 수정")
+    @PatchMapping("/{productModuleId}")
+    public ResponseEntity<ApiResponse<ProductModuleResponse>> updateProductModule(
+            @PathVariable("productModuleId") Long productModuleId,
+            @RequestBody ProductModuleRequest request
+    ) {
+        ProductModuleResponse response = productModuleService.update(productModuleId, request);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
 }
