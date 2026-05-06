0df7fd36264f37a2d2f6219b7c1385274accbeea
최준 <choijun0627@naver.com>
2026-05-06 13:02:26 +0900

    [S14P31S106-207] feat: CompanyManager 엔티티에 필드 추가
    - CompanyManager 엔티티에 name, email, mobilePhone, officePhone, department, position, role 필드를 추가했습니다.
    - 각 필드에 대해 제약 조건을 설정하여 데이터 무결성을 강화했습니다.
    - 해당 변경은 백엔드 (Java, Spring Boot) 영역에서 이루어졌습니다.

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/company/entity/CompanyManager.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/company/entity/CompanyManager.java
@@ -1,6 +1,7 @@
 package com.nkia.Orbis.domain.company.entity;
 
 import com.nkia.Orbis.common.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,12 +10,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SQLRestriction("deleted = false")
 public class CompanyManager extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +30,22 @@ public class CompanyManager extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id")
     private Company company;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    private String mobilePhone;
+
+    private String officePhone;
+
+    @Column(length = 50)
+    private String department;
+
+    private String position;
+
+    private String role;
+
 }
