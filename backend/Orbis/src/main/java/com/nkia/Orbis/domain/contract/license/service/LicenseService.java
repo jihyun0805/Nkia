1d10a73b298634fc4e4c0b293b17246f1b841624
여희림 <gmlfla0219@naver.com>
2026-05-07 16:47:19 +0900

    [S14P31S106-183] feat:  License  단독 생성 Service 구현

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/contract/license/service/LicenseService.java
@@ -0,0 +1,45 @@
+package com.nkia.Orbis.domain.contract.license.service;
+
+import com.nkia.Orbis.common.exception.ApiException;
+import com.nkia.Orbis.common.exception.errorcode.CompanyErrorCode;
+import com.nkia.Orbis.common.exception.errorcode.ProductModuleErrorCode;
+import com.nkia.Orbis.domain.company.entity.Company;
+import com.nkia.Orbis.domain.company.repository.CompanyRepository;
+import com.nkia.Orbis.domain.contract.license.dto.request.LicenseRequest;
+import com.nkia.Orbis.domain.contract.license.dto.response.LicenseResponse;
+import com.nkia.Orbis.domain.contract.license.entity.License;
+import com.nkia.Orbis.domain.contract.license.repository.LicenseRepository;
+import com.nkia.Orbis.domain.productmodule.entity.ProductModule;
+import com.nkia.Orbis.domain.productmodule.repository.ProductModuleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LicenseService {
+
+    private final LicenseRepository licenseRepository;
+    private final CompanyRepository companyRepository;
+    private final ProductModuleRepository productModuleRepository;
+
+    public LicenseResponse create(LicenseRequest request) {
+        Company customerCompany = companyRepository.findById(request.getCustomerCompanyId())
+                .orElseThrow(() -> new ApiException(CompanyErrorCode.COMPANY_NOT_FOUND));
+
+        ProductModule productModule = productModuleRepository.findById(request.getProductModuleId())
+                .orElseThrow(() -> new ApiException(ProductModuleErrorCode.PRODUCT_MODULE_NOT_FOUND));
+
+        License license = License.create(
+                customerCompany,
+                productModule,
+                request.getQuantity(),
+                request.getLicenseType(),
+                request.getStartDate(),
+                request.getEndDate()
+        );
+
+        License savedLicense = licenseRepository.save(license);
+
+        return LicenseResponse.from(savedLicense);
+    }
+}
