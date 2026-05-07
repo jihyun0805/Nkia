d72d379b5baf7d85a7df381a15e0cc596dfd9ce7
여희림 <gmlfla0219@naver.com>
2026-05-08 08:47:37 +0900

    [S14P31S106-219] feat: Department Repository 생성

---
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/admin/department/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.nkia.Orbis.domain.admin.department.repository;
+
+import com.nkia.Orbis.domain.admin.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
