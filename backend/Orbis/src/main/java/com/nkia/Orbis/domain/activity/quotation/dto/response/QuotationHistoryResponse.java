0abe8b212de30d6e68aaaa2b35f0984b07ea2fc4
여희림 <gmlfla0219@naver.com>
2026-05-10 10:19:36 +0900

    [S14P31S106-150] feat: QuotationHistory 목록 조회  구현

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/activity/quotation/dto/response/QuotationHistoryResponse.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/activity/quotation/dto/response/QuotationHistoryResponse.java
@@ -1,4 +1,26 @@
 package com.nkia.Orbis.domain.activity.quotation.dto.response;
 
+import com.nkia.Orbis.domain.activity.quotation.entity.QuotationHistory;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class QuotationHistoryResponse {
+    private Long id;
+
+    private String quotationCode;
+
+    private Integer version;
+
+    private LocalDate quotationDate;
+
+    public static QuotationHistoryResponse from(QuotationHistory quotationHistory) {
+        return QuotationHistoryResponse.builder()
+                .id(quotationHistory.getId())
+                .version(quotationHistory.getVersion())
+                .quotationDate(quotationHistory.getQuotationDate())
+                .build();
+    }
 }
