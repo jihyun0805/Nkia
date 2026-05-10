968c012dbdfe3372265a6bf4c0386b5a9ed3811d
여희림 <gmlfla0219@naver.com>
2026-05-10 18:33:57 +0900

    [S14P31S106-222] feat: 권한 생성 로직 구현

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/admin/permission/service/RoleService.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/admin/permission/service/RoleService.java
@@ -1,7 +1,9 @@
 package com.nkia.Orbis.domain.admin.permission.service;
 
 import com.nkia.Orbis.common.exception.ApiException;
+import com.nkia.Orbis.common.exception.errorcode.AuthErrorCode;
 import com.nkia.Orbis.common.exception.errorcode.UserErrorCode;
+import com.nkia.Orbis.domain.admin.permission.dto.request.RoleCreateRequest;
 import com.nkia.Orbis.domain.admin.permission.dto.request.RolePermissionRequest;
 import com.nkia.Orbis.domain.admin.permission.dto.response.RoleResponse;
 import com.nkia.Orbis.domain.admin.permission.entity.Permission;
@@ -11,6 +13,7 @@ import com.nkia.Orbis.domain.admin.permission.repository.RoleRepository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,5 +44,24 @@ public class RoleService {
         role.changePermissions(permissions);
 
         return RoleResponse.from(role);
+    }
+
+    @Transactional
+    public RoleResponse create(RoleCreateRequest request) {
+        if (roleRepository.findByName(request.getName()).isPresent()) {
+            throw new ApiException(AuthErrorCode.ROLE_ALREADY_EXISTS);
+        }
+
+        Set<Permission> permissions = request.getPermissions().stream()
+                .map(permissionItem -> permissionRepository.findByDomainAndAction(
+                        permissionItem.getDomain(),
+                        permissionItem.getAction()
+                ).orElseThrow(() -> new ApiException(AuthErrorCode.PERMISSION_NOT_FOUND)))
+                .collect(Collectors.toSet());
+
+        Role role = Role.create(request.getName());
+        role.changePermissions(permissions);
+
+        return RoleResponse.from(roleRepository.save(role));
     }
 }
