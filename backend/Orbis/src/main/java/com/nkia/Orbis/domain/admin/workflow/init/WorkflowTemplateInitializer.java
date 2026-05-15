a95a5e247ece8fbe6d0efd20afb7827e83fd982f
여희림 <gmlfla0219@naver.com>
2026-05-15 11:49:49 +0900

    [S14P31S106-341] feat: Workflow Domain 추가

--- backend/Orbis/src/main/java/com/nkia/Orbis/domain/admin/workflow/init/WorkflowTemplateInitializer.java
+++ backend/Orbis/src/main/java/com/nkia/Orbis/domain/admin/workflow/init/WorkflowTemplateInitializer.java
@@ -32,6 +32,9 @@ public class WorkflowTemplateInitializer implements CommandLineRunner {
         createTwoStepWorkflow(WorkflowDomain.PURCHASE_CONTRACT, "매입 계약 결재");
         createTwoStepWorkflow(WorkflowDomain.FREE_MAINTENANCE_CONTRACT, "무상유지보수 계약 결재");
         createTwoStepWorkflow(WorkflowDomain.PAID_MAINTENANCE_CONTRACT, "유상유지보수 계약 결재");
+        createTwoStepWorkflow(WorkflowDomain.PRB, "PRB 보고서 결재");
+        createTwoStepWorkflow(WorkflowDomain.PRB_RESULT, "PRB 결과 보고서 결재");
+        createTwoStepWorkflow(WorkflowDomain.BID_RESULT, "입찰 결과 결재");
 
         createThreeStepWorkflow(
                 WorkflowDomain.LICENSE,
